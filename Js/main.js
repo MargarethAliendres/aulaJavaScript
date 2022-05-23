18935d4df9cdb7860a9e9b15c967c76b18b05972
Margareth Aliendres <margareth.aliendres@gmail.com>
2022-05-22 21:32:58 -0300

    alteracao

---
+++ Js/main.js
@@ -0,0 +1,100 @@
+
+/*                  PRIMEIRA PARTE DE JAVA SCRIPT
+/*var nome = "Margareth Aliendres";
+var idade1 = 40;    
+var idade2 = 10;
+var frase = "Japao e o melhor lugar do mundo";
+
+alert(idade1 + idade2);
+
+console.log(nome);
+console.log(idade1+idade2);
+console.log(frase.replace("Japao", "Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
+alert(frase.replace("Japao", "Brasil"));
+
+/*console.log(frase);  apresenta o nome do japao com o acrescenta o replace muda para brasil*/  
+/*var idade1 = "40";     com aspas os numeros : será apresentado 4010
+var idade2 = "10";
+alert(idade1 + idade2);
+
+/*var idade1 = 40;   sem aspas os numeros : sera apresentado 50
+var idade2 = 10;   
+alert(idade1 + idade2);
+
+alert( idade1 + idade2);   soma idades
+alert(nome + " tem " + idade + " anos"); apresenta mensagem completa: margareth aliendrse tem 40 anos*/
+
+
+        /*Array*/
+        
+//var lista = ["maca", "pera", "laranja"];
+//lista.push("uva");   //inseri item na lista
+//lista.pop ();      tira o ultimo item da lista no caso da uva que foi inserido pelo push
+//console.log(lista.length);
+//console.log(lista.reverse()); //inverte a lista itens ao contrario
+//console.log(lista.join(" "));  a lista fica como uma string entre as aspas pode colocar * | espaço nada etc
+//console.log(lista.toString()); a lista fica como uma string
+//console.log(lista[1]); aparece somente a pera  a contagem comeca de 0
+//alert(lista);
+
+        //array  - dicionario  (igual objeto jason)
+ /*var fruta = {nome:"maça", cor:"vermelha"};
+console.log(fruta);
+console.log(fruta.nome);
+console.log(fruta.cor);
+alert(fruta.nome + " , " + fruta.cor); */
+
+       // array - lista de dicionario   inicia com colchete e fecha com colvhete
+//var frutas = [{nome:"maça", cor:"vermelha"}, {nome: "uva", cor: "verde"}];
+//console.log(frutas);
+//console.log(frutas[1].nome);
+//alert(frutas[1].nome);
+
+
+
+            //condicionais, laços de repeticao, e date
+
+              // condicional
+//var idade = prompt("qual é sua idade?");    usa essa ou 
+//var idade = 18;                            essa
+//console.log(idade);
+//    if (idade > 18) {
+//    alert("Maior de idade")
+//    }else {
+//    alert("Menor de idade")
+//}
+
+             //laço de repeticao
+ 
+ //var count = 0;
+   //while (count <5) {            while-enquanto
+   // console.log(count);   
+   // count = count + 1;
+//ou 
+   //count++;
+  // alert(count);
+//   };
+   
+//var count;
+//   for (count=0; count <= 5; count++) {     for - para
+ //       alert(count);
+ //       console.log(count);
+ //  };
+
+            //DATE (DATA)
+    
+var d = new Date();
+//alert(d);
+//alert(d.getMonth()+1);
+//alert(d.getMinutes());
+  alert(d.getHours());
+console.log(d.getMonth()+1);
+
+
+
+   
+    
+
